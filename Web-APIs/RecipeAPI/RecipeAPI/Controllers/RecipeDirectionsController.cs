a4800cab9977f8edeaf901148367030a1f8607f2
Ali-Faisal <alifaisalas99@gmail.com>
2021-05-22 21:26:45 +0500

    recipe-ing/dir-endpoint

---
+++ Web-APIs/RecipeAPI/RecipeAPI/Controllers/RecipeDirectionsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecipeAPI.Models;
+
+namespace RecipeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeDirectionsController : ControllerBase
+    {
+        private readonly RecipeDatabaseContext _context;
+
+        public RecipeDirectionsController(RecipeDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/RecipeDirections
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RecipeDirection>>> GetRecipeDirection()
+        {
+            return await _context.RecipeDirection.ToListAsync();
+        }
+
+        // GET: api/RecipeDirections/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RecipeDirection>> GetRecipeDirection(int id)
+        {
+            var recipeDirection = await _context.RecipeDirection.FindAsync(id);
+
+            if (recipeDirection == null)
+            {
+                return NotFound();
+            }
+
+            return recipeDirection;
+        }
+
+        // PUT: api/RecipeDirections/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRecipeDirection(int id, RecipeDirection recipeDirection)
+        {
+            if (id != recipeDirection.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(recipeDirection).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RecipeDirectionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/RecipeDirections
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<RecipeDirection>> PostRecipeDirection(RecipeDirection recipeDirection)
+        {
+            _context.RecipeDirection.Add(recipeDirection);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (RecipeDirectionExists(recipeDirection.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetRecipeDirection", new { id = recipeDirection.Id }, recipeDirection);
+        }
+
+        // DELETE: api/RecipeDirections/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<RecipeDirection>> DeleteRecipeDirection(int id)
+        {
+            var recipeDirection = await _context.RecipeDirection.FindAsync(id);
+            if (recipeDirection == null)
+            {
+                return NotFound();
+            }
+
+            _context.RecipeDirection.Remove(recipeDirection);
+            await _context.SaveChangesAsync();
+
+            return recipeDirection;
+        }
+
+        private bool RecipeDirectionExists(int id)
+        {
+            return _context.RecipeDirection.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Web-APIs/RecipeAPI/RecipeAPI/Controllers/RecipeIngredientsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecipeAPI.Models;
+
+namespace RecipeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeIngredientsController : ControllerBase
+    {
+        private readonly RecipeDatabaseContext _context;
+
+        public RecipeIngredientsController(RecipeDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/RecipeIngredients
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RecipeIngredient>>> GetRecipeIngredient()
+        {
+            return await _context.RecipeIngredient.ToListAsync();
+        }
+
+        // GET: api/RecipeIngredients/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RecipeIngredient>> GetRecipeIngredient(int id)
+        {
+            var recipeIngredient = await _context.RecipeIngredient.FindAsync(id);
+
+            if (recipeIngredient == null)
+            {
+                return NotFound();
+            }
+
+            return recipeIngredient;
+        }
+
+        // PUT: api/RecipeIngredients/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRecipeIngredient(int id, RecipeIngredient recipeIngredient)
+        {
+            if (id != recipeIngredient.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(recipeIngredient).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RecipeIngredientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/RecipeIngredients
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<RecipeIngredient>> PostRecipeIngredient(RecipeIngredient recipeIngredient)
+        {
+            _context.RecipeIngredient.Add(recipeIngredient);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (RecipeIngredientExists(recipeIngredient.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetRecipeIngredient", new { id = recipeIngredient.Id }, recipeIngredient);
+        }
+
+        // DELETE: api/RecipeIngredients/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<RecipeIngredient>> DeleteRecipeIngredient(int id)
+        {
+            var recipeIngredient = await _context.RecipeIngredient.FindAsync(id);
+            if (recipeIngredient == null)
+            {
+                return NotFound();
+            }
+
+            _context.RecipeIngredient.Remove(recipeIngredient);
+            await _context.SaveChangesAsync();
+
+            return recipeIngredient;
+        }
+
+        private bool RecipeIngredientExists(int id)
+        {
+            return _context.RecipeIngredient.Any(e => e.Id == id);
+        }
+    }
+}
