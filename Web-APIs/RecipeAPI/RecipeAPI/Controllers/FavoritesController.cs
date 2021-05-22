b8f5315a98c7c83973efc97ec6e62a9d6b70a825
Ali-Faisal <alifaisalas99@gmail.com>
2021-05-22 21:08:27 +0500

    fvrt-endpoint

---
+++ Web-APIs/RecipeAPI/RecipeAPI/Controllers/FavoritesController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecipeAPI.Models;
+
+namespace RecipeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FavoritesController : ControllerBase
+    {
+        private readonly RecipeDatabaseContext _context;
+
+        public FavoritesController(RecipeDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Favorites
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Favorites>>> GetFavorites()
+        {
+            return await _context.Favorites.ToListAsync();
+        }
+
+        // GET: api/Favorites/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Favorites>> GetFavorites(int id)
+        {
+            var favorites = await _context.Favorites.FindAsync(id);
+
+            if (favorites == null)
+            {
+                return NotFound();
+            }
+
+            return favorites;
+        }
+
+        // PUT: api/Favorites/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFavorites(int id, Favorites favorites)
+        {
+            if (id != favorites.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(favorites).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FavoritesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Favorites
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Favorites>> PostFavorites(Favorites favorites)
+        {
+            _context.Favorites.Add(favorites);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (FavoritesExists(favorites.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetFavorites", new { id = favorites.Id }, favorites);
+        }
+
+        // DELETE: api/Favorites/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Favorites>> DeleteFavorites(int id)
+        {
+            var favorites = await _context.Favorites.FindAsync(id);
+            if (favorites == null)
+            {
+                return NotFound();
+            }
+
+            _context.Favorites.Remove(favorites);
+            await _context.SaveChangesAsync();
+
+            return favorites;
+        }
+
+        private bool FavoritesExists(int id)
+        {
+            return _context.Favorites.Any(e => e.Id == id);
+        }
+    }
+}
