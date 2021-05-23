015c97da3683ae4201f86eab7a93ad9a12110e90
AimanSiddiqui111 <aimansiddiqui@folio3.com>
2021-05-23 17:20:15 +0500

    Added Ingredients Control API

---
+++ Web-APIs/RecipeAPI/RecipeAPI/Controllers/IngredientsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecipeAPI.Models;
+
+namespace RecipeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IngredientsController : ControllerBase
+    {
+        private readonly RecipeDatabaseContext _context;
+
+        public IngredientsController(RecipeDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Ingredients
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Ingredient>>> GetIngredient()
+        {
+            return await _context.Ingredient.ToListAsync();
+        }
+
+        // GET: api/Ingredients/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Ingredient>> GetIngredient(int id)
+        {
+            var ingredient = await _context.Ingredient.FindAsync(id);
+
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            return ingredient;
+        }
+
+        // PUT: api/Ingredients/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutIngredient(int id, Ingredient ingredient)
+        {
+            if (id != ingredient.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(ingredient).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!IngredientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Ingredients
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Ingredient>> PostIngredient(Ingredient ingredient)
+        {
+            _context.Ingredient.Add(ingredient);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (IngredientExists(ingredient.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetIngredient", new { id = ingredient.Id }, ingredient);
+        }
+
+        // DELETE: api/Ingredients/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Ingredient>> DeleteIngredient(int id)
+        {
+            var ingredient = await _context.Ingredient.FindAsync(id);
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            _context.Ingredient.Remove(ingredient);
+            await _context.SaveChangesAsync();
+
+            return ingredient;
+        }
+
+        private bool IngredientExists(int id)
+        {
+            return _context.Ingredient.Any(e => e.Id == id);
+        }
+    }
+}
