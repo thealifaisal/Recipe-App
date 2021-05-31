ef1616d28ce19aa1db8e71e45b239b94f04abe2d
AimanSiddiqui111 <aimansiddiqui@folio3.com>
2021-06-01 00:10:15 +0500

    Added the http configuration

--- Web-APIs/RecipeAPI/RecipeAPI/Startup.cs
+++ Web-APIs/RecipeAPI/RecipeAPI/Startup.cs
@@ -12,12 +12,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using RecipeAPI.Models;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.AspNetCore.Authentication.Certificate;
 
 namespace RecipeAPI
 {
     public class Startup
     {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -28,9 +29,22 @@ namespace RecipeAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string connection = Configuration.GetConnectionString("RecipeDatabase");
+            var connection = Configuration.GetConnectionString("RecipeDatabase");
             services.AddDbContextPool<RecipeDatabaseContext>(options => options.UseSqlServer(connection));
+
+            services.AddCors(options => {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                              builder =>
+                              {
+                                  builder.WithOrigins("http://localhost:3000",
+                                                      "https://localhost:3000");
+                              });
+            });
             services.AddControllers();
+            services.AddAuthentication(
+            CertificateAuthenticationDefaults.AuthenticationScheme)
+            .AddCertificate();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,11 +55,12 @@ namespace RecipeAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(MyAllowSpecificOrigins);
             app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
