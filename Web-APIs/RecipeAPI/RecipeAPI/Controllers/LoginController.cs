6449af994459188f45b4d8fb7915e1113c3c4a40
Ali-Faisal <alifaisalas99@gmail.com>
2021-05-31 05:49:59 +0500

    replaced-with-Login-API

---
+++ Web-APIs/RecipeAPI/RecipeAPI/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecipeAPI.Models;
+
+namespace RecipeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly RecipeDatabaseContext _context;
+
+        public LoginController(RecipeDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Login/5/123456
+        [HttpGet("{id}/{password}")]
+        public async Task<ActionResult<User>> AuthUserGET(string id, string password)
+        {
+            var user = await _context.User.FindAsync(id);
+
+            if (user == null || !user.Password.Equals(password))
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+    }
+}
